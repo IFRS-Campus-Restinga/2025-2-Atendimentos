824d3f3205cf66eac46a5b84f7d1b9dbde7f8d7a
root <root@DESKTOP-MDN68C5.>
2025-09-18 21:52:34 -0300

    Criando template basico de login

--- frontent/app/src/App.jsx
+++ frontent/app/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import './index.css'
+
 
 function App() {
-  
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/appointments" element={<h1>Página de Atendimentos</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
