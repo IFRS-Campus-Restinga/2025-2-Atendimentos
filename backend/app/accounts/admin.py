4b9d5fbeb730535a7c7f380bda52a24d11888941
JefersonAreas <2018007239@restinga.ifrs.edu.br>
2025-09-25 00:14:59 -0300

    criação do model disciplina/ crud

--- backend/app/accounts/admin.py
+++ backend/app/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models.disciplina import Disciplina
 
 # Register your models here.
+
+admin.site.register(Disciplina)
---
+++ backend/app/services/serializers/__init__.py
@@ -0,0 +1,1 @@
+from .disciplina_serializer import *
