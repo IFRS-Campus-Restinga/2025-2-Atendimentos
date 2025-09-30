040da03d8997fdda28f719f344bc9ed4ad563925
Pedro <gunlarksas31@gmail.com>
2025-09-30 20:53:56 -0300

    Eliminando arquivos duplicados (cursoSerializer -> curso_serializer)

--- backend/app/accounts/enumerations/TipoUsuario.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class TipoPerfil(models.TextChoices):
-
-    ADMIN = 'ADM', 'Administrador'
-    PROFESSOR = 'PROF', 'Professor'
-    ALUNO = 'ALU', 'Aluno'
-    COORDENADOR = 'COOR', 'Coordenador'
