318fb14eb7f12a29e2863918cbf53a026f997a95
Pedro <gunlarksas31@gmail.com>
2025-10-11 10:57:39 -0300

    Alterado seed - disciplina

--- backend/app/accounts/management/commands/seed.py
+++ backend/app/accounts/management/commands/seed.py
@@ -91,7 +91,6 @@ class Command(BaseCommand):
                 disciplina, _ = Disciplina.objects.get_or_create(
                     nome=nome_disciplina,
                     codigo=codigo_disciplina,
-                    descricao=f"Descrição da disciplina {nome_disciplina}",
                 )
                 disciplina.cursos.add(curso)  
                 self.stdout.write(self.style.SUCCESS(f'Disciplina: {disciplina.nome}'))
