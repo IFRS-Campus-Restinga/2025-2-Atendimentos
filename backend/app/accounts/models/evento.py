6414e6ed14c159327b71d8a0e77ac2671598c67a
Andressa da Rosa Lopes <andressa.lopes@facta.com.br>
2025-11-05 11:07:45 -0300

    116 - Refatoração de evento e evento ordinário para realizar recorrência semanal do evento

--- backend/app/accounts/models/evento.py
+++ backend/app/accounts/models/evento.py
@@ -6,11 +6,16 @@ from .usuario import Usuario
 
 class Evento(BaseModel):
 
-    dia_semana = models.DateField(
-        help_text="Data do dia da semana do evento"
-        )
+    data_criacao = models.DateTimeField(
+        default=timezone.now,
+        blank=True,
+        help_text="Data e hora de criação do evento" )
 
-    data_hora = models.DateTimeField(
+    data_evento = models.DateField(
+        default=timezone.now,
+        help_text="Data do evento" )
+        
+    data_hora_evento = models.TimeField(
         default=timezone.now,
         help_text="Data e hora do evento")
 
