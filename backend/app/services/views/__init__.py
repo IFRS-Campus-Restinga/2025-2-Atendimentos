88a544447f6222d45e8461dc2ecc86b032626d90
Maicon Da Matta <91135489+Maicon308@users.noreply.github.com>
2025-11-16 12:23:31 -0300

    Ajustado __init__.py em services.views
    Atualizado para refletir mudanças nas views e manter a organização do módulo.

--- backend/app/services/views/__init__.py
+++ backend/app/services/views/__init__.py
@@ -9,4 +9,3 @@ from .professor_views_set import *
 from .turma_views_set import *
 from .usuario_view_set import *
 from .registro_atendimento_views_set import *
-from .historico_atendimento_views import *
