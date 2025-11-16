3e442e4d28ae0adf6eb85f89bec130ef60e112d5
Maicon Da Matta <91135489+Maicon308@users.noreply.github.com>
2025-11-16 12:22:03 -0300

    Ajustado __init__.py em services.serializers
    Atualizado para refletir mudanças nos serializers e manter a organização do módulo.

--- backend/app/services/serializers/__init__.py
+++ backend/app/services/serializers/__init__.py
@@ -12,5 +12,4 @@ from .curso_coordenador_resumido_serializer import *
 from .evento_extraordinario_serializer import *
 from .registro_atendimento_serializer import *
 from .complemento_cadastro_serializer import *
-from .historico_atendimento_serializer import * 
 from .complemento_cadastro_serializer import *
